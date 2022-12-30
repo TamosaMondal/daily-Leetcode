172da953937bde291b2bda97ead6df3bebaa148a
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2022-12-30 21:05:10 +0530

    medium level

--- Maximum Bags With Full Capacity of Rocks.java
+++ Maximum Bags With Full Capacity of Rocks.java
@@ -1,6 +1,6 @@
-You have n bags numbered from 0 to n - 1. You are given two 0-indexed integer arrays capacity and rocks. The ith bag can hold a maximum of capacity[i] rocks and currently contains rocks[i] rocks. You are also given an integer additionalRocks, the number of additional rocks you can place in any of the bags.
+/*You have n bags numbered from 0 to n - 1. You are given two 0-indexed integer arrays capacity and rocks. The ith bag can hold a maximum of capacity[i] rocks and currently contains rocks[i] rocks. You are also given an integer additionalRocks, the number of additional rocks you can place in any of the bags.
 
-Return the maximum number of bags that could have full capacity after placing the additional rocks in some bags.
+Return the maximum number of bags that could have full capacity after placing the additional rocks in some bags.*/
 
 
 
