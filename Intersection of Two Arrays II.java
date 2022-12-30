4a9c1e49f9b4fae33e0d64ce0085334832a3ba9f
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2022-12-30 21:03:35 +0530

    in java

--- Intersection of Two Arrays II.java
+++ Intersection of Two Arrays II.java
@@ -1,4 +1,4 @@
-Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+/*Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.*/
 
 
 class Solution {
