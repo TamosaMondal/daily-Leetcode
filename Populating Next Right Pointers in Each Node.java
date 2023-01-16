e364a78b92b18703affa679446a615f1f745c830
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-01-16 23:32:05 +0530

    3ms||Java||EasyToUnderstand💥

---
+++ Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,67 @@
+/*You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root==null){
+            return null;
+        }
+        Queue<Node>q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node cur=q.poll();
+            if(cur==null && q.isEmpty()){
+                return root;
+                
+            }
+            else if(cur==null){
+                q.add(null);
+                continue;
+            }
+            else{
+                cur.next=q.peek();
+                if(cur.left!=null){
+                    q.add(cur.left);
+                }
+                if(cur.right!=null){
+                    q.add(cur.right);
+                }
+            }
+        }
+    return root;
+    }
+}
