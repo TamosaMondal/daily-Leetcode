b5f271da5b7b4662fe72a64b5390bd8dc88b65e4
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-01-20 16:20:37 +0530

    Mosteasy

---
+++ Minimum Remove to Make Valid Parentheses.java
@@ -0,0 +1,40 @@
+/*Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+*/
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        char ch[]=s.toCharArray();
+        Stack<Integer>st=new Stack<>();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]=='(' || ch[i]==')'){
+                if(ch[i]=='(')
+                    st.push(i);
+                else if(ch[i]==')' && !st.isEmpty()){
+                    st.pop();
+                }
+                else{
+                    ch[i]='@';
+                }
+            }
+
+        }
+        while(!st.isEmpty()){
+            ch[st.pop()]='@';
+        }
+        String ans="";
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]!='@'){
+                ans+=ch[i];
+            }
+        }
+        return ans;
+    }
+}
