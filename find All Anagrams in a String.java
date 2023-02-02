83655240ec94056af8f5751ae8a0430c6d442c8c
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-02-02 20:53:35 +0530

    Easy To Understand||Java✌🔥

---
+++ find All Anagrams in a String.java
@@ -0,0 +1,36 @@
+/*Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+*/
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res=new ArrayList<>();
+        Map<Character, Integer> pHash = new HashMap<>();
+        Map<Character, Integer> hash = new HashMap<>();
+        for (int i = 0; i < p.length(); i++) {
+            pHash.put(p.charAt(i), pHash.getOrDefault(p.charAt(i), 0) + 1);
+        }
+        int left = 0, right = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            hash.put(c, hash.getOrDefault(c, 0) + 1);
+            right++;
+            if (pHash.equals(hash)) {
+                res.add(left);
+            }
+            if (right - left == p.length()) {
+                char leftChar = s.charAt(left);
+                if (hash.containsKey(leftChar)) {
+                    hash.put(leftChar, hash.get(leftChar) - 1);
+                    if (hash.get(leftChar) == 0) {
+                        hash.remove(leftChar);
+                    }
+                }
+                left++;
+            }
+        }
+
+        return res;
+    }
+}
