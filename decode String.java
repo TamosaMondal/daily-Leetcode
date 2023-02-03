ff0ef8894ccfa70bd05aceba886acd75aab8991c
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-02-03 15:59:30 +0530

    0ms||Easy||Java

---
+++ decode String.java
@@ -0,0 +1,39 @@
+/*Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+*/
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer>numStack=new Stack<>();
+        Stack<StringBuilder>strBuild=new Stack<>();
+        StringBuilder str = new StringBuilder();
+        int num=0;
+        for(char c:s.toCharArray()){
+            if(c>='0' && c<='9'){
+                num=num*10 +c -'0';
+            }
+            else if(c=='['){
+                strBuild.push(str);
+                str=new StringBuilder();
+                numStack.push(num);
+                num=0;
+            }else if(c==']'){
+                StringBuilder temp=str;
+                str=strBuild.pop();
+                int count=numStack.pop();
+                while(count-->0){
+                    str.append(temp);
+                }
+            }else{
+                str.append(c);
+            }
+        }
+        return str.toString();
+
+    }
+}
