dd42fc6e74f6e936ea91e60b13482be2542202a9
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-03-10 09:31:35 +0530

    📢💥Very Easy Solution||Java

---
+++ Linked List Random Node.java
@@ -0,0 +1,38 @@
+/*Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Implement the Solution class:
+
+Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
+int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+ 
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ArrayList<Integer>list=new ArrayList<>();
+    public Solution(ListNode head) {
+        while(head!=null){
+            list.add(head.val);
+            head=head.next;
+        }
+    }
+    
+    public int getRandom() {
+        double random=Math.random()*list.size();
+        return list.get((int)random);
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
