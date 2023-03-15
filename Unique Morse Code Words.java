bf113af5e477328986a4d5a008c28071d422c164
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-03-15 09:28:49 +0530

    ✌✌Easy & Short Solution

---
+++ Unique Morse Code Words.java
@@ -0,0 +1,29 @@
+/*International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+*/
+
+class Solution {
+    private String transform(String str) {
+        String s[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."},res="";
+        for(int i=0;i<str.length();i++)
+            res+=s[(int)str.charAt(i)-'a'];
+        return res;
+    }
+    public int uniqueMorseRepresentations(String[] words) {
+        HashSet<String> set = new HashSet<String>();
+        for(String w : words) {
+            set.add(transform(w));
+        }
+        return set.size();
+    }
+}
