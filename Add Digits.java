e546e2b41b369d931063cb9546a9ee94594c04d2
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-03-21 17:54:52 +0530

    ✌😁Super Easy Solution

---
+++ Add Digits.java
@@ -0,0 +1,9 @@
+/*Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+*/
+class Solution {
+    public int addDigits(int num) {
+        if(num==0) return 0;
+        else if(num%9==0) return 9;
+        else return num%9;
+    }
+}
