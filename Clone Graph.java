68bc37db6c9bd0f9e2b33863d8b4d8d25e9eeb78
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-04-08 15:12:12 +0530

    💥🧨DFS Solution||Easy

---
+++ Clone Graph.java
@@ -0,0 +1,55 @@
+/*Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Node, Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        if (map.containsKey(node)){
+            return map.get(node);
+        }
+        map.put(node, new Node(node.val));//if the node is not copy yet,copy it
+        for (Node val : node.neighbors) {//go to the neibours node
+            map.get(node).neighbors.add(cloneGraph(val));
+        }
+        return map.get(node);
+    }
+}
