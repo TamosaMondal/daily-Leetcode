761593218b78653a3ebbe5d07b63d76e05bbd12f
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-04-23 13:46:49 +0530

    🔥Easy Solution

---
+++ Calculate Delayed Arrival Time.java
@@ -0,0 +1,15 @@
+/*You are given a positive integer arrivalTime denoting the arrival time of a train in hours, and another positive integer delayedTime denoting the amount of delay in hours.
+
+Return the time when the train will arrive at the station.
+
+Note that the time in this problem is in 24-hours format.
+*/
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int ans=arrivalTime+delayedTime;
+        if(ans>=24){
+            return ans-24;
+        }
+        return ans;
+    }
+}
