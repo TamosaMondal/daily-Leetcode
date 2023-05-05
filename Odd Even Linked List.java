043f5638b7a3694c49ab9af0a7528ac41fad1300
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-05-05 13:25:08 +0530

    📢💥Two pointers||Easy

---
+++ Odd Even Linked List.java
@@ -0,0 +1,34 @@
+/*Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head ==null || head.next==null) return head;
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode evenStart=even;
+        while(odd!=null && odd.next!=null && even!=null && even.next!=null){
+            odd.next = odd.next.next;
+            odd = even.next;
+            even.next = odd.next;
+            even = odd.next;
+        }
+        odd.next = evenStart;
+        return head;
+    }
+}
