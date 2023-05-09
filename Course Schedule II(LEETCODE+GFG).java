2d7ad47bec66bd82de454d20a736b832b713a4b3
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-05-09 13:49:18 +0530

    Create Course Schedule II(LEETCODE+GFG).java
    🔥✔KAHN'S Algorithm||BFS||Easy

---
+++ Course Schedule II(LEETCODE+GFG).java
@@ -0,0 +1,111 @@
+/*                                         <---LEETCODE SOLUTION--->
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+*/
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList < ArrayList < Integer >>adj=new ArrayList<>();
+
+        for(int i=0;i<=numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<prerequisites.length;i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+
+        Queue<Integer>q=new LinkedList<>();
+        int indegree[]=new int[numCourses];
+        
+        for(int i=0;i<numCourses;i++){
+            for(int e:adj.get(i)){
+                indegree[e]++;
+            }
+        }
+        
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        
+        int ans[]=new int[numCourses];
+        int i=0;
+        
+        while(!q.isEmpty()){
+            int node=q.peek();
+            q.poll();
+            ans[i++]=node;
+            
+            for(int e:adj.get(node)){
+                indegree[e]--;
+                if(indegree[e]==0) q.add(e);
+            }
+        }
+        if(i==numCourses) return ans;
+        int arr[]={};
+        return arr;
+    }
+}
+/*                                                  <---GFG SOLUTION--->
+There are a total of n tasks you have to pick, labeled from 0 to n-1. Some tasks may have prerequisites tasks, for example to pick task 0 you have to first finish tasks 1, which is expressed as a pair: [0, 1]
+Given the total number of n tasks and a list of prerequisite pairs of size m. Find a ordering of tasks you should pick to finish all tasks.
+Note: There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all tasks, return an empty array. Returning any correct order will give the output as 1, whereas any invalid order will give the output "No Ordering Possible".
+*/
+class Solution
+{
+    static int[] findOrder(int n, int m, ArrayList<ArrayList<Integer>> prerequisites) 
+    {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            adj.get(prerequisites.get(i).get(1)).add(prerequisites.get(i).get(0));
+        }
+
+
+
+        int indegree[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+
+        Queue<Integer> q = new LinkedList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+
+        int topo[] = new int[n];
+        int ind = 0;
+        // o(v + e)
+        while (!q.isEmpty()) {
+            int node = q.peek();
+
+            q.remove();
+            topo[ind++] = node;
+            // node is in your topo sort
+            // so please remove it from the indegree
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.add(it);
+            }
+        }
+
+
+        if (ind == n) return topo;
+        int[] arr = {};
+        return arr;
+    }
+}
